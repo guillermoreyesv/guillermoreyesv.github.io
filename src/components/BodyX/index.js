6232749ec95c7419c9c7d7eb64728c919b9f9aa2
Guillermo <guillermo_hard@hotmail.com>
2023-03-04 17:24:51 -0600

    addimaes

--- src/components/BodyX/index.js
+++ src/components/BodyX/index.js
@@ -1,43 +1,52 @@
 import React from 'react';
 import './Body.css';
+import avatar from '../../images/avatar.webp';
+import python from '../../images/python.webp';
+import postgres from '../../images/postgres.webp';
+import php from '../../images/php.webp';
+import mysql from '../../images/mysql.webp';
+import mongodb from '../../images/mongodb.webp';
+import js from '../../images/js.webp';
+import docker from '../../images/docker.webp';
+import github from '../../images/github.webp';
 
 function Body() {
+  const initYear = 2018;
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  const expYears = currentYear - initYear
+
   return (
     <main>
       <section id="sobre-mi">
         <div className="flex-container">
           <div className="item-img">
-            <img className='avatar' src='https://avatars.githubusercontent.com/u/38173795?v=4'/>
+            <img rel="prefetch" className='avatar' src={avatar}/>
           </div>
           <div className="item">
-            <h2>Guillermo Reyes</h2>
-            <p>¡Hola! Mi nombre es [tu nombre] y soy un desarrollador de software con [número] años de experiencia. Me apasiona crear aplicaciones web y móviles que hagan la vida de las personas más fácil.</p>
+            <h1>Guillermo Reyes</h1>
+            <h2 className='button-especial'>Back-end Developer</h2>
+            <div className='description'>
+              <p>
+              Prefiero ser breve asi que resumiendo tengo {expYears} años de experiencia en el desarrollo software y estas son algunas de las tecnologias que manejo: 
+              </p>
+            </div>
+            <div className='img-technologies'>
+              <img title='python' alt='python' src={python}/>
+              <img title='php' alt='php' src={php}/>
+              <img title='js' alt='js' src={js}/>
+              <img title='postgres' alt='postgres' src={postgres}/>
+              <img title='mysql' alt='mysql' src={mysql}/>
+              <img title='mongodb' alt='mongodb' src={mongodb}/>
+              <img title='docker' alt='docker' src={docker}/>
+              <img title='github' alt='github' src={github}/>
+            </div>
           </div>
         </div>
       </section>
       <section id="proyectos">
         <h2>Proyectos</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean feugiat orci non consequat posuere. Etiam ullamcorper justo sed mollis iaculis. Pellentesque et suscipit lorem. Proin libero lectus, tristique ut eros facilisis, mollis consequat nulla. Nullam vitae ex ultrices, tempus enim sit amet, laoreet lectus. Fusce sit amet lacus at elit aliquam mattis. Curabitur interdum urna in rutrum feugiat. Etiam egestas a nibh eu bibendum. Praesent eu velit tristique, lacinia purus eu, sollicitudin urna. Proin id lectus sit amet enim scelerisque efficitur. Ut pellentesque imperdiet turpis, at vehicula arcu condimentum quis. Morbi luctus scelerisque justo, vitae mollis dolor elementum a.
-
-Mauris consectetur in justo eu ultrices. Mauris volutpat accumsan sem nec dignissim. In facilisis imperdiet tellus in condimentum. Sed convallis sodales neque nec faucibus. Pellentesque nec nulla tempor, iaculis lectus id, semper lacus. Sed sodales eu purus vel condimentum. Phasellus suscipit lorem sit amet consequat malesuada. Phasellus sed volutpat est. Sed quis viverra diam, non mollis erat. Nam aliquet molestie aliquam. Fusce non maximus libero.
-
-Ut eget velit vestibulum, tempus diam in, placerat dolor. In sit amet volutpat nibh. Donec mauris libero, imperdiet vitae quam at, luctus euismod mi. Praesent consequat auctor justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam faucibus porta mi ut imperdiet. Ut convallis leo sem.
-
-Nulla mollis interdum ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis non enim justo. Sed id libero pulvinar lacus fermentum pellentesque sit amet non lorem. Nulla sed nunc tempus nisi porttitor dapibus. In vulputate diam tortor, in porta dui efficitur sed. Cras ac ex non massa egestas malesuada. Proin eu sapien vulputate, mollis ligula at, semper metus. Integer dictum vulputate eros in imperdiet. Sed eu vestibulum lacus. Nulla vel dolor tristique, fermentum lacus vel, commodo eros. Nunc sagittis bibendum urna, sed finibus est euismod vitae.
-
-Aliquam lobortis posuere nisl, ut commodo risus. Quisque nec blandit arcu. Aliquam congue justo a nibh pellentesque sagittis. Integer vel dignissim elit, a tempus lacus. Aliquam metus leo, aliquam in molestie porttitor, accumsan quis ligula. Nulla facilisi. Nam est sem, finibus eget pulvinar ut, dictum et turpis. Aliquam erat volutpat. Sed imperdiet auctor purus ut ultrices. Donec lectus ligula, ultricies a feugiat id, ornare eu odio. Aliquam erat volutpat. In posuere congue erat non tristique. Nullam in porttitor arcu. Quisque vestibulum dui eu tincidunt hendrerit.
-
-Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce dictum purus felis, sit amet porta mi sollicitudin in. Donec tempor odio ac massa bibendum dignissim. Aliquam turpis lectus, dictum nec viverra eu, placerat in enim. Pellentesque sit amet purus non lectus consequat facilisis. Maecenas molestie nulla a magna iaculis imperdiet. Sed condimentum libero diam. Vivamus vitae pretium turpis. Ut ac neque sed leo molestie tempor. In varius libero sit amet leo molestie rhoncus. Pellentesque eu pharetra diam, id accumsan quam. Aliquam ornare suscipit mollis. Aliquam fringilla libero neque. Phasellus iaculis efficitur elit quis fermentum. Aenean molestie tellus eget tellus ornare, ut elementum diam eleifend.
-
-In fermentum quis quam sit amet scelerisque. Ut scelerisque egestas massa vitae suscipit. Donec in lacinia ante. Cras tincidunt, neque sed porttitor posuere, mauris est accumsan dui, eu pharetra magna nisi eu est. Nunc at venenatis augue. Pellentesque sodales viverra dui non porta. Sed luctus arcu at mi tristique hendrerit et eget mauris. Integer tincidunt, lacus sit amet rutrum vulputate, lorem sem tempus massa, a volutpat augue sem id dui. Quisque eget porta lorem. Vivamus eu ex ex.
-
-Maecenas rutrum ut enim a viverra. Donec molestie neque at odio tincidunt elementum. Morbi suscipit tristique accumsan. Sed nulla arcu, mollis in finibus ac, porta malesuada neque. Curabitur pulvinar interdum ex. Ut fermentum mollis ante. Ut id eros elementum, finibus turpis vel, scelerisque ipsum. Duis sed tortor sit amet turpis porta lacinia. Curabitur euismod facilisis bibendum. Vivamus at sapien convallis, tristique libero quis, bibendum augue. Nunc rutrum malesuada ligula. Donec et egestas lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce tortor nibh, tempus eu est vel, sagittis commodo massa. Nulla mollis purus accumsan efficitur auctor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-
-Nunc rhoncus ex eu ligula interdum, a condimentum felis hendrerit. Sed nec urna ac urna egestas bibendum. Fusce iaculis efficitur sapien, vitae venenatis dui vehicula vel. Nunc sed dapibus massa. Aenean ac erat at mauris dictum posuere ut ut diam. Duis aliquet at quam sit amet eleifend. Sed id libero interdum, venenatis dui vitae, sollicitudin elit. Vivamus auctor mauris vel placerat elementum. Nam vel placerat tortor. Nam dignissim sem eget tincidunt interdum. Duis odio tortor, interdum vel ex a, fringilla finibus nibh. Aenean in neque id sapien ullamcorper molestie nec quis ante. Cras viverra vehicula lorem id volutpat. Pellentesque nibh purus, euismod eu porttitor at, placerat ut eros. Suspendisse hendrerit, turpis sed imperdiet pharetra, ex arcu feugiat quam, eu ornare elit libero tempor nibh.
-
-Suspendisse eros est, tempor vel purus convallis, iaculis tincidunt velit. Nam mi erat, porta nec risus ac, laoreet faucibus elit. Donec quis sapien nibh. Aenean vehicula magna lectus, non efficitur lacus vestibulum at. Ut egestas porta metus at semper. Pellentesque odio justo, pellentesque egestas malesuada nec, sollicitudin ac nisl. Curabitur id pharetra neque, quis consequat nibh. Aenean nec vulputate erat. Proin hendrerit, erat sit amet convallis euismod, nunc nulla tempor augue, eu mattis erat massa eu quam.
-
-</p>
+        <p></p>
         <ul>
           <li><a href="#">Proyecto 1</a></li>
           <li><a href="#">Proyecto 2</a></li>
